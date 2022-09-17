86ab28af365e261b013b1c14100bfe6d463cab59
Lorenzo Verducci <verduccii46@gmail.com>
2022-09-17 18:32:05 +0200

    Get UserID

--- backend/routes/user_corporate.js
+++ backend/routes/user_corporate.js
@@ -1,6 +1,9 @@
 const User = require('../models/user.model');
 const Corporate = require('../models/corporate.model');
+const OfferedService = require('../models/offered.service');
+const RequiredService = require('../models/required.service');
 const express = require('express');
+const { ObjectID } = require('bson');
 
 const router = express.Router();
 
@@ -27,6 +30,42 @@ router.route('/list-corporates').get(async (req,res) => {
             res.status(200).json(_result);
         }
     )
+})
+
+router.route('/:idPost/get-user-id').get(async (req,res) => {
+    const idPost = req.params.idPost;
+    await OfferedService.getOfferedServices().findOne({ _id: ObjectID(idPost) }, async (err,result) => {
+        if (err) handelError(err,res);
+        const _result = await result;
+        if (!_result) {
+            await RequiredService.getRequiredServices().findOne({ _id: ObjectID(idPost) }, async (err,result) => {
+                if (err) handelError(err,res);
+                const _result_ = await result;
+                if (!_result_) return res.status(404).send('Post not found!');
+                else {
+                    await User.getUser().findOne({ username: _result_.user }, async (err,result) => {
+                        if (err) handelError(err,res);
+                        const us = await result;
+                        return res.status(200).send(us._id);
+                    })
+                }
+            })
+        } else {
+            await User.getUser().findOne({ username: _result.user }, async (err,result) => {
+                if (err) handelError(err,res);
+                const us = await result;
+                if (!us) {
+                    await Corporate.getCorporates().findOne({ name: _result.user }, async (err,result) => {
+                        if (err) handelError(err,res);
+                        const corp = await result;
+                        return res.status(200).send(corp._id);
+                    })
+                } else {
+                    return res.status(200).send(us._id);
+                }
+            })
+        }
+    })
 })
 
 module.exports = router;
