ba1d011c58cf3cd7e68841303ce7f0961d92d06a
girogiosld <giorgiosaldana99@gmail.com>
2022-10-25 20:00:44 +0200

    fixed name in code

--- frontend/src/pages/ServiceUserPage.js
+++ frontend/src/pages/ServiceUserPage.js
@@ -78,7 +78,7 @@ const ServiceUserPage = () => {
                 if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
                     data = decrypt(localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY))
                     info = JSON.parse(data)
-                } else  if (sessionStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+                } else if (sessionStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
                     data = decrypt(sessionStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY))
                     info = JSON.parse(data)
                 }
